ac89b822b036b0345bc2243e4e2977f7e25db816
unknown <wagesve@.(none)>
2014-06-21 11:57:04 +0200

    Creating phpUnit Tests

--- testing/main.php
+++ testing/main.php
@@ -1,78 +1,55 @@
 <?php
 
-class FirstTest extends PHPUnit_Extensions_SeleniumTestCase{
+class SurveyvalTests extends PHPUnit_Extensions_SeleniumTestCase{
+		
+	var $browser_url = 'http://localhost/surveyval/';
+	var $is_local = FALSE;
+	
+	var $browser = '*firefox';
+	var $users = array(
+		'test1' => 'test1',
+		'test2' => 'test2',
+		'test3' => 'test3',
+		'test4' => 'test4',
+		'test5' => 'test5',
+		'test6' => 'test6',
+	);
+	
+	var $db_data = array(
+		'host' => 'localhost',
+		'user' => 'root',
+		'pass' => '',
+		'db'   => 'surveyval',
+		'wpdb_prefix' => 'wpsrvv_'
+	);
+	
 	public function setUp(){
-		$this->setHost( 'localhost' );
-		$this->setPort( 4444 );
-		$this->setBrowser( 'firefox' );
-		$this->setBrowserUrl( 'http://plugins/' );
+		$this->setBrowser( $this->browser );
+		$this->setBrowserUrl( $this->browser_url );
 	}
 	
 	public function test(){
-		$this->open( "wp-login.php" );
-		$this->type( "id=user_login", "test" );
-		$this->type( "id=user_pass", "test" );
-		$this->click( "id=wp-submit" );
-	    $this->waitForPageToLoad( "30000" );
+		$con = $this->connect_db();
 		
-		$this->open("/survey/kopie-umfrage-melle-2/");
-	    $this->click("name=surveyval_response[101][]");
-	    $this->click("document.surveyval.elements['surveyval_response[101][]'][8]");
-	    $this->click("document.surveyval.elements['surveyval_response[101][]'][16]");
-	    $this->click("document.surveyval.elements['surveyval_response[101][]'][24]");
-	    $this->click("document.surveyval.elements['surveyval_response[101][]'][32]");
-	    $this->click("document.surveyval.elements['surveyval_response[101][]'][40]");
-	    $this->click("document.surveyval.elements['surveyval_response[101][]'][48]");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->select("name=surveyval_response[105]", "label=7");
-	    $this->type("name=surveyval_response[106]", "111");
-	    $this->select("name=surveyval_response[109]", "label=7");
-	    $this->type("name=surveyval_response[110]", "111");
-	    $this->select("name=surveyval_response[113]", "label=3");
-	    $this->type("name=surveyval_response[114]", "25");
-	    $this->type("name=surveyval_response[115]", "2");
-	    $this->type("name=surveyval_response[117]", "180");
-	    $this->type("name=surveyval_response[118]", "110");
-	    $this->select("name=surveyval_response[119]", "label=Ja");
-	    $this->select("name=surveyval_response[120]", "label=Weniger als 10");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][8]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][15]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][22]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][29]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][36]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][46]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][57]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][66]");
-	    $this->click("document.surveyval.elements['surveyval_response[122][]'][75]");
-	    $this->click("name=surveyval_submission_back");
-	    $this->waitForPageToLoad("30000");
-	    $this->click("name=surveyval_submission_back");
-	    $this->waitForPageToLoad("30000");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->type("name=surveyval_response[110]", "11167");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->type("name=surveyval_response[110]", "1112");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->click("name=surveyval_submission_back");
-	    $this->waitForPageToLoad("30000");
-	    $this->click("name=surveyval_submission_back");
-	    $this->waitForPageToLoad("30000");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->type("name=surveyval_response[117]", "18011");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->type("name=surveyval_response[117]", "180");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->click("name=surveyval_submission");
-	    $this->waitForPageToLoad("30000");
-	    $this->assertEquals("Danke für die Teilnahme an der Umfrage!", $this->getText("//div[@id='surveyval-thank-participation']"));
+		foreach( $this->users as $user_name => $user_pass ):
+			$this->open( "wp-login.php" );
+			$this->type( "id=user_login", $user_name );
+			$this->type( "id=user_pass", $user_pass );
+			$this->click( "id=wp-submit" );
+			
+		    $this->waitForPageToLoad( "30000" );
+		endforeach;
+	}
+	
+	private function connect_db(){
+		$con = mysqli_connect( $this->db_data[ 'host' ], $this->db_data[ 'user' ], $this->db_data[ 'pass' ], $this->db_data[ 'db' ] );
+		
+		// Check connection
+		if ( mysqli_connect_errno()) {
+		  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			return FALSE;
+		}
+		
+		return $con;
 	}
 }
